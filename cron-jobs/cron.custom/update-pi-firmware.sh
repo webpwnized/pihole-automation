815063e508546ad0abd220f4425524e615ff7b88
webpwnized <webpwnized@gmail.com>
2025-07-05 21:34:47 -0400

    1.0.1 Add install script

--- cron-jobs/cron.custom/update-pi-firmware.sh
+++ cron-jobs/cron.custom/update-pi-firmware.sh
@@ -15,13 +15,21 @@ log_error() {
 trap 'log_error $LINENO' ERR
 
 log "=============================="
-log "Raspberry Pi Firmware Update"
+log "Raspberry Pi Firmware & Kernel Update"
 log "=============================="
 
 log "Updating Raspberry Pi firmware using apt..."
 sudo apt update
 sudo apt full-upgrade -y
-sudo rpi-eeprom-update -a
+sudo apt autoremove -y
+sudo apt autoclean -y
+
+if ! command -v rpi-eeprom-update >/dev/null 2>&1; then
+  log "WARNING: rpi-eeprom-update not found. Skipping EEPROM update."
+else
+  log "Updating bootloader EEPROM..."
+  sudo rpi-eeprom-update -a
+fi
 
 log "SUCCESS: Firmware packages updated. A reboot may be required."
 
